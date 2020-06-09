e28929f06a0b2405c403586ca12d6be31686fee9
hatter222 <56596758+hatter222@users.noreply.github.com>
2020-06-09 22:30:56 +0200

    Added label to display command

--- My_img_app.py
+++ My_img_app.py
@@ -1,8 +1,9 @@
 import sys
 from PyQt5 import QtWidgets, QtGui,QtCore
-from ImageProcessingTool.Mainwindow import Ui_Form
+from Mainwindow import Ui_Form
 import cv2
 from PyQt5.QtGui import QImage ,QPixmap
+from PyQt5.QtWidgets import QErrorMessage
 
 
 class Mainwindow(QtWidgets.QMainWindow):
@@ -12,26 +13,34 @@ class Mainwindow(QtWidgets.QMainWindow):
         self.load_flag = False
         self.image=None
         self.gray=None
+        self.msg= None
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+        self.initUI()
+
+    def initUI(self):
+
         self.ui.Load.clicked.connect(self.slot_load)
         self.ui.Gray_scale.clicked.connect(self.slot_gray)
         self.ui.Mean.clicked.connect(self.slot_mean)
         self.ui.Median.clicked.connect(self.slot_median)
         self.ui.Gauss.clicked.connect(self.slot_gauss)
+        self.text = None
 
     def slot_load(self):
         print("load")
         self.image =cv2.imread('lena2.jpg',1)
         img = self.conv2Qimage()
         self.load_flag = True
+        self.gray_flag = True
         #img = QtGui.QImage(QtGui.QImageReader("lena2.jpg").read())
         pixmap = QPixmap.fromImage(img)
         h = self.ui.Screen_1.height()
         w = self.ui.Screen_1.width()
         self.ui.Screen_1.setPixmap(pixmap.scaled(w,h,QtCore.Qt.KeepAspectRatio))
         self.ui.Screen_2.setPixmap(pixmap.scaled(w,h,QtCore.Qt.KeepAspectRatio))
-
+        self.text = "Load"
+        self.ui.usedcommand.setText(self.text)
 
     def slot_gray(self):
         print("gray")
@@ -46,39 +55,82 @@ class Mainwindow(QtWidgets.QMainWindow):
             w = self.ui.Screen_1.width()
             self.ui.Screen_1.setPixmap(pixmap.scaled(w, h, QtCore.Qt.KeepAspectRatio))
             self.gray = image
+            self.text = self.text+"--> gray"
+            self.ui.usedcommand.setText(self.text)
+            self.gray_flag = True
+        else :
+            self.msg ="Image is not loaded"
+            self.display()
 
     def slot_mean(self):
         print("mean")
-        num = self.getkernel()
-        blur = cv2.blur(self.gray, (num,num))
-        height, width, depth = self.image.shape
-        img = QImage(blur.data, width, height, width, QImage.Format_Grayscale8)
-        pixmap = QPixmap.fromImage(img)
-        h = self.ui.Screen_1.height()
-        w = self.ui.Screen_1.width()
-        self.ui.Screen_1.setPixmap(pixmap.scaled(w, h, QtCore.Qt.KeepAspectRatio))
+        if self.gray_flag and self.load_flag:
+            num = self.getkernel()
+            blur = cv2.blur(self.gray, (num, num))
+            height, width, depth = self.image.shape
+            img = QImage(blur.data, width, height, width, QImage.Format_Grayscale8)
+            pixmap = QPixmap.fromImage(img)
+            h = self.ui.Screen_1.height()
+            w = self.ui.Screen_1.width()
+            self.ui.Screen_1.setPixmap(pixmap.scaled(w, h, QtCore.Qt.KeepAspectRatio))
+            self.text = "gray" + "--> Mean with kernel-" + str(num)
+            self.ui.usedcommand.setText(self.text)
+
+        elif not self.load_flag:
+            self.msg ="Image not loaded"
+            self.display()
+        else :
+            self.msg = "Convert Image to Grayscale Image"
+            self.display()
+
 
     def slot_median(self):
         print("median")
-        num = self.getkernel()
-        blur = cv2.medianBlur(self.gray,num)
-        height, width, depth = self.image.shape
-        img = QImage(blur.data, width, height, width, QImage.Format_Grayscale8)
-        pixmap = QPixmap.fromImage(img)
-        h = self.ui.Screen_1.height()
-        w = self.ui.Screen_1.width()
-        self.ui.Screen_1.setPixmap(pixmap.scaled(w, h, QtCore.Qt.KeepAspectRatio))
+        if self.gray_flag:
+            num = self.getkernel()
+            blur = cv2.medianBlur(self.gray,num)
+            height, width, depth = self.image.shape
+            img = QImage(blur.data, width, height, width, QImage.Format_Grayscale8)
+            pixmap = QPixmap.fromImage(img)
+            h = self.ui.Screen_1.height()
+            w = self.ui.Screen_1.width()
+            self.ui.Screen_1.setPixmap(pixmap.scaled(w, h, QtCore.Qt.KeepAspectRatio))
+
+            self.text = "Gray" + "--> Median with kernel-" + str(num)
+            self.ui.usedcommand.setText(self.text)
+
+        elif not self.load_flag:
+            self.msg = "Image not loaded"
+            self.display()
+
+        else:
+            self.msg = "Convert Image to Grayscale Image"
+            self.display()
+
+
+
 
     def slot_gauss(self):
         print("gauss")
-        num = self.getkernel()
-        blur = cv2.GaussianBlur(self.gray, (num,num),0)
-        height, width, depth = self.image.shape
-        img = QImage(blur.data, width, height, width, QImage.Format_Grayscale8)
-        pixmap = QPixmap.fromImage(img)
-        h = self.ui.Screen_1.height()
-        w = self.ui.Screen_1.width()
-        self.ui.Screen_1.setPixmap(pixmap.scaled(w, h, QtCore.Qt.KeepAspectRatio))
+        if self.gray_flag:
+            num = self.getkernel()
+            blur = cv2.GaussianBlur(self.gray, (num,num),0)
+            height, width, depth = self.image.shape
+            img = QImage(blur.data, width, height, width, QImage.Format_Grayscale8)
+            pixmap = QPixmap.fromImage(img)
+            h = self.ui.Screen_1.height()
+            w = self.ui.Screen_1.width()
+            self.ui.Screen_1.setPixmap(pixmap.scaled(w, h, QtCore.Qt.KeepAspectRatio))
+
+            self.text = "gray" + "--> GaussianBlur with kernel-" + str(num)
+            self.ui.usedcommand.setText(self.text)
+        elif not self.load_flag:
+            self.msg = "Image not loaded"
+            self.display()
+
+        else :
+            self.msg = "Convert Image to Grayscale Image"
+            self.display()
 
     def conv2Qimage(self):
         height, width, depth = self.image.shape
@@ -91,6 +143,13 @@ class Mainwindow(QtWidgets.QMainWindow):
         if ok:
             return num
 
+    def update_command(attach, self=None):
+        self.text = self.text + str(attach)
+        self.ui.usedcommand.setText(self.text)
+    def display(self):
+        error_dialog = QtWidgets.QErrorMessage()
+        error_dialog.showMessage(self.msg)
+        error_dialog.exec()
 
 
 app = QtWidgets.QApplication(sys.argv)
